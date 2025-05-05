400ea7e3417b5c4d280fd471e0718eae4ee7e286
rteachey <ricky@teachey.org>
2025-05-05 14:15:30 -0400

    chose generic style of immutable copyable

--- utils/copyable.py
+++ utils/copyable.py
@@ -27,17 +27,3 @@ class ImmutableCopyable(Generic[T]):
 
         # Create new instance
         return self.__class__.model_validate(current_data)
-
-
-# Alternative: Mixin approach
-class ImmutableCopyableMixin:
-    """Mixin for immutable models that can create copies with changes."""
-
-    def with_changes(self, **changes):
-        """Create a new instance with specified changes."""
-        current_data = self.model_dump()
-        for key, value in changes.items():
-            if key not in current_data:
-                raise ValueError(f"Invalid field: {key}")
-            current_data[key] = value
-        return self.__class__.model_validate(current_data)
